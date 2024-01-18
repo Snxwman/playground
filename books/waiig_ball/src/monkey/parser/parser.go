5572e21e3e574688bccbe72af80975e04cb8beea
Sam Kopp <github@snxwman.dev>
2024-01-17 20:12:09 -0500

    writing an interpreter in go, chapter 2 (day 2)

--- books/waiig_ball/src/monkey/parser/parser.go
+++ books/waiig_ball/src/monkey/parser/parser.go
@@ -7,6 +7,22 @@ import (
 	"monkey/token"
 )
 
+const (
+    _ int = iota
+    LOWEST
+    EQUALS
+    LESSGREATER
+    SUM
+    PRODUCT
+    PREFIX
+    CALL
+)
+
+type (
+    prefixParseFn func() ast.Expression
+    infixParseFn func(ast.Expression) ast.Expression
+)
+
 type Parser struct {
     l *lexer.Lexer
 
@@ -14,6 +30,9 @@ type Parser struct {
     peekToken token.Token
     
     errors []string
+
+    prefixParseFns map[token.TokenType]prefixParseFn
+    infixParseFns map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -43,14 +62,6 @@ func (p *Parser) nextToken() {
     p.peekToken = p.l.NextToken()
 }
 
-func (p *Parser) curTokenIs(t token.TokenType) bool {
-    return p.curToken.Type == t
-}
-
-func (p *Parser) peekTokenIs(t token.TokenType) bool {
-    return p.peekToken.Type == t
-}
-
 func (p *Parser) expectPeek(t token.TokenType) bool {
     if p.peekTokenIs(t) {
         p.nextToken()
@@ -59,6 +70,21 @@ func (p *Parser) expectPeek(t token.TokenType) bool {
         p.peekError(t)
         return false
     }
+}
+func (p *Parser) curTokenIs(t token.TokenType) bool {
+    return p.curToken.Type == t
+}
+
+func (p *Parser) peekTokenIs(t token.TokenType) bool {
+    return p.peekToken.Type == t
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+    p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+    p.infixParseFns[tokenType] = fn
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -85,8 +111,20 @@ func (p *Parser) parseStatement() ast.Statement {
     case token.RETURN:
         return p.parseReturnStatement()
     default:
+        return p.parseExpressionStatement() 
+    }
+}
+
+func (p *Parser) parseExpression() ast.Expression {
+    prefix := p.prefixParseFns[p.curToken.Type]
+
+    if prefix == nil {
         return nil
     }
+
+    leftExp := prefix()
+
+    return leftExp
 }
 
 func (p *Parser) parseLetStatement() *ast.LetStatement {
@@ -115,6 +153,18 @@ func (p *Parser) parseReturnStatement() *ast.ReturnStatement {
     p.nextToken()
 
     if !p.expectPeek(token.SEMICOLON) {
+        p.nextToken()
+    }
+
+    return stmt
+}
+
+func (p *Parser) parseExpressionStatement() *ast.ExpressionStatement {
+    stmt := &ast.ExpressionStatement{Token: p.curToken}
+
+    stmt.Expression = p.parseExpression(LOWEST)
+
+    if p.peekTokenIs(token.SEMICOLON) {
         p.nextToken()
     }
 
